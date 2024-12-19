0ba004a9b4316fe6f9a72c32f724070d6f191d3d
Betül Tuğçe Dikdoğmuş <betultugcedikdogmus@gmail.com>
2024-12-20 00:36:29 +0300

    22.ders tamamlandi.

---
+++ CSharpEgitimKampi501/CSharpEgitimKampi501/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using CSharpEgitimKampi501.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi501.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllAsync();
+        Task CreateAsync(CreateProductDto createProductDto);
+        Task UpdateAsync(UpdateProductDto updateProductDto);
+        Task DeleteAsync(int id);
+        Task<ResultProductDto> GetByIdAsync(int id);
+    }
+}
---
+++ CSharpEgitimKampi501/CSharpEgitimKampi501/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using CSharpEgitimKampi501.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi501.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateAsync(CreateProductDto createProductDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ResultProductDto> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(UpdateProductDto updateProductDto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
